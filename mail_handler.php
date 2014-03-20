c042b262a2204d738c765771a769e66846ad37f4
foughter <foughter@gmail.com>
2014-03-20 00:45:34 -0700

    Feedback work

---
+++ mail_handler.php
@@ -0,0 +1,21 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "contact@danxingdao.org"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['first_name'];
+    $subject = "User Feedback " . $name;
+    $subject2 = "Copy of your form submission";
+    //$message = $from . " " . $name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message = $name . "(" . $from . ") wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+    //$message = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
+?>
