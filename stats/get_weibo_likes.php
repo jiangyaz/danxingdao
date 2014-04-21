da86dd997b408540343172ba1d1718ade7435f43
foughter <foughter@gmail.com>
2014-04-20 21:31:11 -0700

    firebase stuff for weibo and fb likes

--- stats/get_weibo_likes.php
+++ stats/get_weibo_likes.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once join('/', array(__DIR__, '../firebase-php', 'firebaseLib.php'));
+
 $uid =  "5040920641";
 $token = "2.00djLJVF0Mp511fe9b9f30b4XzFW_B";
 $url = "https://api.weibo.com/2/users/counts.json?uids=$uid&access_token=$token";
@@ -7,6 +9,8 @@ $url = "https://api.weibo.com/2/users/counts.json?uids=$uid&access_token=$token"
 $info_arr =  json_decode(file_get_contents($url));
 $follower_num = $info_arr[0]->{'followers_count'};
 
-echo $follower_num;
+$firebase = new Firebase('https://radiant-fire-8364.firebaseio.com/');
+$res_num = $firebase->set('likes/weibo_follows', $follower_num);
+echo $res_num;
 ?>
 
