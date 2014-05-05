a2244938ffd662c599168275dd2a958e7fbc8442
jiangyaz <jiangyaz@usc.edu>
2014-05-05 14:34:05 -0700

    updating the update time.

---
+++ stats/get_current_time.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set('America/Los_Angeles');
+require_once join('/', array(__DIR__, '../firebase-php', 'firebaseLib.php'));
+
+$time = date('m/d/Y, h:i:s a', time());
+
+$firebase = new Firebase('https://radiant-fire-8364.firebaseio.com/');
+$res_num = $firebase->set('likes/update_time', $time);
+echo $res_num;
+?>
