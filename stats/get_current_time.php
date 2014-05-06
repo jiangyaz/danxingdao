fd0617706d8330b843018b7a7420735616fe8397
jiangyaz <jiangyaz@usc.edu>
2014-05-05 19:06:12 -0700

    update chinese website half done.

--- stats/get_current_time.php
+++ stats/get_current_time.php
@@ -2,7 +2,7 @@
 date_default_timezone_set('America/Los_Angeles');
 require_once join('/', array(__DIR__, '../firebase-php', 'firebaseLib.php'));
 
-$time = date('m/d/Y, h:i:s a', time());
+$time = date('m/d/Y, h:i a', time());
 
 $firebase = new Firebase('https://radiant-fire-8364.firebaseio.com/');
 $res_num = $firebase->set('likes/update_time', $time);
