132a31482c127440db2f16087b356c9889e2dcdf
jiangyaz <jiangyaz@usc.edu>
2014-05-22 12:52:18 -0700

    updating reposts script.

--- stats/get_weibo_reposts.php
+++ stats/get_weibo_reposts.php
@@ -5,17 +5,31 @@ require_once join('/', array(__DIR__, '../firebase-php', 'firebaseLib.php'));
 $uid   =  "5040920641";
 $token = "2.00djLJVF0Mp511fe9b9f30b4XzFW_B";
 
-$reposts_url ="https://api.weibo.com/2/statuses/user_timeline.json?access_token=2.00djLJVF0Mp511fe9b9f30b4XzFW_B&count=200";
-$reposts_info =  json_decode(file_get_contents($reposts_url));
-$reposts_arr = $reposts_info->{'statuses'};
-
+$page_num = 1;
 $total_reposts = 0;
 
-for ($i = 0; $i < count($reposts_arr); $i++) {
-	$post = $reposts_arr[$i];
-	// echo $i.": ".$post->{'reposts_count'}."\n";
-	$total_reposts = $total_reposts + $post->{'reposts_count'};
+while(true) {
+	$reposts_url ="https://api.weibo.com/2/statuses/user_timeline.json?access_token=2.00djLJVF0Mp511fe9b9f30b4XzFW_B&page=".$page_num;
+	$reposts_info =  json_decode(file_get_contents($reposts_url));
+	$reposts_arr = $reposts_info->{'statuses'};
+
+	if (count($reposts_arr) == 0) {
+		break;
+	}
+
+	$page_reposts = 0;
+	for ($i = 0; $i < count($reposts_arr); $i++) {
+		$post = $reposts_arr[$i];
+		// echo $i.": ".$post->{'id'}." ".$post->{'reposts_count'}."\n";
+		$page_reposts = $page_reposts + $post->{'reposts_count'};
+	}
+
+	$total_reposts = $total_reposts + $page_reposts;
+	echo "page ".$page_num.", reposts:".$page_reposts."\n";
+	$page_num = $page_num + 1;
 }
+
+echo $total_reposts."\n";
 
 if ($total_reposts != NULL){
 	$firebase = new Firebase('https://radiant-fire-8364.firebaseio.com/');
